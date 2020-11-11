f47e600b4c0981e4bcb0e6039449d742aaa80068
RisingVirtuesBiggestFan <49250026+RisingVirtuesBiggestFan@users.noreply.github.com>
2020-11-11 13:20:53 -0800

    Restricted input, optimized autocomplete
    Tidied up styling

--- script.js
+++ script.js
@@ -2,26 +2,28 @@ var itemList;
 var rawMaterialList;
 var searchInput;
 var recipeList;
-
+var enterInput;
+var database = [
+        "Holy Rainbow Gloves",
+        "Maple Lumber",
+        "Maple Clogs",
+        "Maple Shortbow",
+        "Bone Harpoon",
+        "Amateur's Grinding Wheel",
+        "Maple Pattens",
+        "Amateur's Spinning Wheel",
+        "Square Maple Shield",
+        "Bronze Spear",
+        "Maple Cane",
+        "Boar Leather",
+        "Raptor Leather",
+        "Aldgoat Leather"
+            ];
 
 $(document).ready(function() {
     itemList = document.getElementById("itemList");
-    var database = [
-        "Holy Rainbow Gloves",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Holy Rainbow",
-        "Aldgoat Leather"
-            ];
+    enterInput = document.getElementById("searchBar");
+    database.sort();
     $("#searchBar").autocomplete({
         source: function(request, response) {
         var results = $.ui.autocomplete.filter(database, request.term);
@@ -29,19 +31,27 @@ $(document).ready(function() {
         response(results.slice(0, 5));
         },
         autoFocus: true,
-        minLength: 3
+        minLength: 2,
+        delay: 100,
     }); 
+ 
+    enterInput.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            document.getElementById("searchButton").click();
+  }
+});
 
 } );
 $("#searchButton").click(function() {
         searchDatabase();
             });
 
+    
+
 
 function searchDatabase() {
-    //future add check to see if input matches database//
     //check to make sure input fields are not blank and create table/append//
-    if ($("#searchBar").val() !=null && $("#searchBar").val() !='') {
+    if ($("#searchBar").val() !=null && $("#searchBar").val() !='' && database.includes($("#searchBar").val())) {
         addItemToTable();
         clearInput();
     }
@@ -50,12 +60,13 @@ function searchDatabase() {
 function addItemToTable() {
     if ($("#itemList tbody").length == 0) {
         $("#itemList").append("<tbody></tbody>");
+        document.getElementById("itemList").style.display = "table";
     }
     $("#itemList tbody").append("<tr>" +
         "<td>" + $("#searchBar").val() + "</td>" +
         "<td>" + "#" + "</td>" +
         "<td>" + "STRING" + "</td>" +
-        "<td>" + "<button type='button' onclick='removeRow(this);'> X </button>" +
+        "<td>" + "<button id='removeButton' type='button' onclick='removeRow(this);'> X </button>" +
         "</td>" +
         "</tr>");
 }
@@ -67,3 +78,5 @@ function clearInput() {
 function removeRow(ctl) {
     $(ctl).parents("tr").remove();
 }
+
+
