9be406bf4f90ce1ff8cf210ca2097f35389c3cbc
RisingVirtuesBiggestFan <49250026+RisingVirtuesBiggestFan@users.noreply.github.com>
2020-11-11 10:26:31 -0800

    Added autocomplete functionality

--- script.js
+++ script.js
@@ -3,16 +3,43 @@ var rawMaterialList;
 var searchInput;
 var recipeList;
 
+
 $(document).ready(function() {
     itemList = document.getElementById("itemList");
-    itemList.innerHTML  = "hey";
-});
+    var database = [
+        "Holy Rainbow Gloves",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Holy Rainbow",
+        "Aldgoat Leather"
+            ];
+    $("#searchBar").autocomplete({
+        source: function(request, response) {
+        var results = $.ui.autocomplete.filter(database, request.term);
 
-    $("#searchButton").click(function() {
+        response(results.slice(0, 5));
+        },
+        autoFocus: true,
+        minLength: 3
+    }); 
+    itemList.innerHTML = database;//putting table using objects here//
+
+} );
+$("#searchButton").click(function() {
         searchDatabase();
-    });
+            });
+
 
 function searchDatabase() {
     searchInput = document.getElementById("searchBar").value;
     itemList.innerHTML = searchInput;
-};
+}
